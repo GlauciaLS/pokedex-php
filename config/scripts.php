6c1f0ccd05c179380e2b8ad6cfe929f5b14cf168
Glaucia Silva <glaucia.ap1258@gmail.com>
2020-06-22 15:37:18 -0300

    card color

--- config/scripts.php
+++ config/scripts.php
@@ -7,6 +7,40 @@ function generateList()
     return $pokemons;
 }
 
+function cardColor($colorType)
+{
+    if($colorType == "Grass") 
+        $color = '#bdecb6';
+    else if($colorType == "Fire") 
+        $color = '#fa7e78';
+    else if($colorType == "Water") 
+        $color = '#add8e6';
+    else if($colorType == "Bug") 
+        $color = '#b7d694';
+    else if($colorType == "Electric") 
+        $color = '#fdfd96';
+    else if($colorType == "Poison") 
+        $color = '#bd86bd';
+    else if($colorType == "Ground") 
+        $color = '#d2b48c';
+    else if($colorType == "Rock") 
+        $color = '#b3a9a1';
+    else if($colorType == "Ghost") 
+        $color = '#a796b3';
+    else if($colorType == "Fighting") 
+        $color = '#ebaf7c';
+    else if($colorType == "Ice") 
+        $color = '#6d9ac9';
+    else if($colorType == "Dragon") 
+        $color = '#c5d6db';
+    else if($colorType == "Psychic") 
+        $color = '#d6b2d5';
+    else
+        $color = '#f0f0f0';
+
+    return $color;
+}
+
 ?>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
--- view/pages/index.php
+++ view/pages/index.php
@@ -9,11 +9,12 @@
         $lista = generateList();
         if(count($lista->pokemon)) {
             foreach($lista->pokemon as $pokemon) {
+                foreach($pokemon->type as $type){ $colorType = $type; break; }
                 ?>
                 <div class="col-sm-2" style="padding-bottom: 1.5%">
-                    <div class="card" style="height: 350px;">
+                    <div class="card" style="border: 2px solid <?php echo cardColor($type);?>; height: 350px; background-color: <?php echo cardColor($type);?>">
                         <a href="pokemon.php?num=<?php echo $pokemon->num?>" class='lang' style="text-decoration: none; color:#000000;"><center><img class="card-img-top" src="<?php echo $pokemon->img?>" alt="<?php echo $pokemon->name?>" style="height: 150px; width: 150px; padding: 10px;"></center>
-                        <div class="card-body">
+                        <div class="card-body" style="background-color: white">
                             <h5 class="card-title" style="text-align: center;"><?php echo $pokemon->name?></h5>
                             <?php
                             $p = "";
